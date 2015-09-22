5c38fa64efa5ab9d39ca17111c8c2d0de7c1fc5e
Frankie <Frankie@Frankies-MacBook-Pro.local>
2015-09-22 13:49:24 -0700

    added professor space

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,6 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/Login', function(req, res, next) {
+  res.render('professorStart', { title: 'Express' });
+});
 
 
 module.exports = router;
